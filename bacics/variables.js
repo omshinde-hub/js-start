4183f3f568b0aa6e9bee97f97866289317e8ba29
omshinde-hub <shindeom130@gmail.com>
2025-03-22 08:31:58 +0000

    container config done

---
+++ bacics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 310705
+let EmailId ="omshinde@gmail.com"
+var accountPassword ="om@123"
+accountCity="mumbai"
+let accountState;
+
+// accountId=3425// not allowed
+//console.log(accountId);
+
+EmailId="shindeom@gmail.com"
+accountPassword="shinde@123"
+accountCity="Ahilyanagar"
+
+/* prefer not use to var
+because of issue in block scope and functional scope */
+
+console.table([accountId,EmailId,accountPassword,accountState])
