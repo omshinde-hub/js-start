58190e485bf208ed8657bab5cb6c28ab2bf44ccb
omshinde-hub <shindeom130@gmail.com>
2025-03-22 09:18:29 +0000

    datatypes

--- bacics/basics.js
+++ bacics/basics.js
@@ -1,1 +1,5 @@
 console.log("om");
+
+
+//https://tc39.es/ecma262/#sec-intro 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
---
+++ bacics/datatype.js
@@ -0,0 +1,33 @@
+"use strict";// treat all js code as a new version
+
+// alert( 6 + 9 )// we are using nodejs, not browser
+
+console.log(3
+    + 3) ; console.log("sawli");
+    // dont write like that code readability should be high
+
+console.log("om")
+console.log(3+3)
+
+//https://tc39.es/ecma262/#sec-ecmascript-data-types-and-values
+
+let name ="om"
+let age=18
+let isLoggedIn = false
+
+
+//number=> 2to power53
+//bigint
+// string =>"om"
+//boolean => true/false
+//null => standalone value
+//undefine =>
+//symbol => unoque
+
+//object
+
+console.log(typeof"om");
+console.log(typeof undefined); // o/p undefined
+console.log(typeof null); //o/p object  ( interview question )
+
+
